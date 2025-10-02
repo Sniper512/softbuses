7c064b17843a1ce53ee2038120bf132286604b20
SoftwareInvictus <abulkalamasif1129@gmail.com>
2025-10-02 23:59:49 +0500

    Fix link target in HeaderContactButton to ensure proper navigation

--- src/components/general/header/HeaderContactButton.jsx
+++ src/components/general/header/HeaderContactButton.jsx
@@ -3,7 +3,7 @@ import { HashLink } from "react-router-hash-link";
 export const HeaderContactButton = () => {
   return (
     <div className="relative group pt-1 pl-1">
-      <HashLink to="#co-create" className="block relative px-4 py-2 border-2 border-white text-primary font-medium transition-all duration-300 cursor-pointer group-hover:-translate-y-1 group-hover:-translate-x-1 group-hover:shadow-[4px_4px_0_0_#69FF00]">
+      <HashLink to="/#co-create" className="block relative px-4 py-2 border-2 border-white text-primary font-medium transition-all duration-300 cursor-pointer group-hover:-translate-y-1 group-hover:-translate-x-1 group-hover:shadow-[4px_4px_0_0_#69FF00]">
         Contact Us
       </HashLink>
     </div>
